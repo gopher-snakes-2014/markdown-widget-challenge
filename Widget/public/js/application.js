67e4aa8d3fa02bd059dbbbdec7d82886a21e4590
Andrew McClellan <andrew.mcclellan3@gmail.com>
2014-10-14 14:33:40 -0700

    Add JS event listener/action

--- Widget/public/js/application.js
+++ Widget/public/js/application.js
@@ -4,4 +4,13 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  function bindEvents() {
+    $(".user_input").on("keyup", function(){
+    codeToTranslate = $(this).val();
+    console.log(codeToTranslate)
+    $("#translation").text(codeToTranslate);
+    })
+  }
+  bindEvents();
 });
