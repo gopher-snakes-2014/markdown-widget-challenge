c3fc6d9b5a9684064c54f61118edd3ee3c8a0fb2
Apprentice <apprentice@master-inf-8.local>
2014-10-14 15:52:13 -0700

    regex poppin

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -1,7 +1,7 @@
 
 $('#text-box').keyup(function(){
   var textContent;
-  textContent = View.getTextContent
-  textContent = Model.translateText(textContent)
-  View.display(textContent)
+  textContent = View.getTextContent();
+  textContent = Model.translateText(textContent);
+  View.display(textContent);
 });
--- javascripts/model.js
+++ javascripts/model.js
@@ -1,5 +1,21 @@
 var Model = {};
 Model.translateText = function(textContent){
-  var translationResult = textContent;
+
+  var translationResult;
+    // regex for shit seperated by **s (\*\*[\w\s]+\*\*)
+
+    var boldyTHings = /(\*\*[^\*]+\*\*)/g;
+    var italicyThings = /[^\*](\*[^\*]+\*)[^\*]/g ;
+
+    translationResult = textContent.match(boldyTHings);
+
+    console.log(translationResult);
+
   return translationResult
 }
+
+
+// while ( ( match = ptrn.exec(input) ) != null )
+// {
+//     alert( match );
+// }
