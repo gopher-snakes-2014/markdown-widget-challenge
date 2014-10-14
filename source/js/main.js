f831e0e0635c0bf8489c186f1b7888786c8d18c5
Joan Torres & Ruskin <pair+gunga.jonnyparris@devbootcamp.com>
2014-10-14 16:35:47 -0700

    input is relfected in output div, live\!

--- source/js/main.js
+++ source/js/main.js
@@ -11,13 +11,26 @@ var View = function(){
 }
 
 var MarkdownWidget = function(source, preview){
-  // source.on(keyUP, function(key) {
-    // preview.render(key)
-  // }
+  $(source).on("keydown", function(e) {
+    if (e.which == 9){
+      e.preventDefault();
+      // position = $(source+" > pre")[0].selectionStart
+      // $(source+" > pre").text($(source+" > pre")
+      $(source).get(0).selectionStart=
+      $(source).get(0).selectionEnd= 1
+    }
+  })
+
   $(source).on("keyup", function(e) {
-    console.log(e.which);
-    /* Act on the event */
+    $(preview).text($(source).text());
   });
+
 }
 
-MarkdownWidget('.input','#output_markdown')
+MarkdownWidget('.input','#output_markdown > p')
+
+
+// Tests
+// delete should be aligned between input and output positions -DONE
+// output should be caps sensitive - DONE
+// tab should insert a tab instead of changing focus - TODO
