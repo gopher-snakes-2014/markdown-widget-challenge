7f6276682f0b21915e2accb7977c59431e1e0ac7
Jay Dave <jaycdave@gmail.com>
2014-10-14 17:17:26 -0700

    added new branch with mvc

--- Widget/public/js/application.js
+++ Widget/public/js/application.js
@@ -1,55 +1,50 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    // controller
 
-    $("#source-id").on("keyup", function(){
-    codeToTranslate = $(this).val();
-    $("#preview-div").append(translator(codeToTranslate));
-    })
+    var MarkdownWidget = function(source,preview){
+      return{
+        $(source).on("keyup", function(){
+        codeToTranslate = $(this).val();
+        view(preview)
+        }
+      });
+    };
 
-    var translator = function(codeToTranslate) {
+    // controller 
+
+    // view
+    var view = function(preview){
+      $(preview).append(translator(codeToTranslate));
+      };
+    });
+    // view
+
+// model
+    var translator = function(codeToTranslate,preview) {
 
        if ((codeToTranslate.charAt(0) === "*") && (codeToTranslate.charAt(1) === "*") && (codeToTranslate.charAt(codeToTranslate.length-1) === "*") && (codeToTranslate.charAt(codeToTranslate.length-2) === "*")) {
-        $("#preview-div").empty();
+        $(preview).empty();
         return "<p><b>" + codeToTranslate.substr(2, (codeToTranslate.length-4)) + "</b></p>"
       }
 
        else if ((codeToTranslate.charAt(0) === "_") && (codeToTranslate.charAt(1) === "_") && (codeToTranslate.charAt(codeToTranslate.length-1) === "_") && (codeToTranslate.charAt(codeToTranslate.length-2) === "_")) {
-        $("#preview-div").empty();
+        $(preview).empty();
         return "<p><b>" + codeToTranslate.substr(2, (codeToTranslate.length-4)) + "</b></p>"
       }
 
       else if ((codeToTranslate.charAt(0) === "*") && (codeToTranslate.charAt(codeToTranslate.length - 1) === "*")) {
-        $("#preview-div").empty();
+        $(preview).empty();
         return "<p><em>" + codeToTranslate.substr(1, (codeToTranslate.length-2)) + "</em></p>"
       }
 
       else if ((codeToTranslate.charAt(0) === "_") && (codeToTranslate.charAt(codeToTranslate.length - 1) === "_")) {
-        $("#preview-div").empty();
+        $(preview).empty();
         return "<p><em>" + codeToTranslate.substr(1, (codeToTranslate.length-2)) + "</em></p>"
       }
       else {
-        $("#preview-div").empty();
+        $(preview).empty();
       }
-
-
-      // } else if (codeToTranslate.charAt(0) === "**") (codeToTranslate.charAt(codeToTranslate.length - 1) === "**")) {{
-      // //   var translatedCode = codeToTranslate.css("color", "red")
-      //   return "Bold"
-
-      // } else if (codeToTranslate.charAt(0) === "_") {
-      //   return "First character is a _"
-      // }
     };
 
 });
-// .replace(/\"/g, "")
-
-//*word* = BOLD word
-
-//  *andrew* or _andrew_ = italics
-//  **andrew** or __andrew__ = bold
-//
