0b5c968052dbb2b1c9996e537afd84bd317be561
Joan Torres & Ruskin <pair+gunga.jonnyparris@devbootcamp.com>
2014-10-14 17:06:18 -0700

    implement single asterisk to italics

--- source/js/main.js
+++ source/js/main.js
@@ -14,15 +14,12 @@ var MarkdownWidget = function(source, preview){
   $(source).on("keydown", function(e) {
     if (e.which == 9){
       e.preventDefault();
-      // position = $(source+" > pre")[0].selectionStart
-      // $(source+" > pre").text($(source+" > pre")
-      $(source).get(0).selectionStart=
-      $(source).get(0).selectionEnd= 1
     }
   })
 
   $(source).on("keyup", function(e) {
-    $(preview).text($(source).text());
+    var text = $(source).text().replace(/\*\b([\w\s]+)\*/gi, "<em>$1</em>")
+    $(preview).html(text);
   });
 
 }
@@ -31,6 +28,4 @@ MarkdownWidget('.input','#output_markdown > p')
 
 
 // Tests
-// delete should be aligned between input and output positions -DONE
-// output should be caps sensitive - DONE
 // tab should insert a tab instead of changing focus - TODO
