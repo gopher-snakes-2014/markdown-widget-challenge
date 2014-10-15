38b055daceac9c3fe9a78e0977319bedfc5e4d80
Joan Torres & Ruskin <pair+gunga.jonnyparris@devbootcamp.com>
2014-10-14 18:33:09 -0700

    Refactors to MVC structure

--- source/js/main.js
+++ source/js/main.js
@@ -1,30 +1,53 @@
-var Model = function() {
-  // body...
+var InputModel = function(source) {
+  this.source = source
+}
+
+InputModel.prototype = {
+  getText: function() {
+    return $(this.source).text()
+  },
+
+  getMarkdownText: function(){
+    return this.getText().replace(/\*\b([\w\s]+)\*/gi, "<em>$1</em>");
+  }
 }
 
 var Controller = function(model, view){
+  this.model = model
+  this.view = view
+}
+
+Controller.prototype = {
+  bindEvents: function() {
+    $(this.model.source).on("keydown", function(e) {
+      if (e.which == 9){
+        e.preventDefault();
+      }
+    });
+
+    $(this.model.source).on("keyup", this.convertToMarkdown.bind(this));
+  },
+
+  convertToMarkdown: function(){
+    var markdownText = this.model.getMarkdownText();
+    this.view.update(markdownText);
+  }
 
 }
 
-var View = function(){
-
+var OutputView = function(preview){
+  this.output = preview
 }
 
-var MarkdownWidget = function(source, preview){
-  $(source).on("keydown", function(e) {
-    if (e.which == 9){
-      e.preventDefault();
-    }
-  })
-
-  $(source).on("keyup", function(e) {
-    var text = $(source).text().replace(/\*\b([\w\s]+)\*/gi, "<em>$1</em>")
-    $(preview).html(text);
-  });
-
+OutputView.prototype = {
+  update: function(input) {
+    $(this.output).html(input);
+  }
 }
 
-MarkdownWidget('.input','#output_markdown > p')
+MarkdownWidget = new Controller(new InputModel('.input'), new OutputView('#output_markdown > p'));
+MarkdownWidget.bindEvents();
+
 
 
 // Tests
