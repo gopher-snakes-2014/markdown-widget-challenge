d84c2cbafeff7eb0ce677bd03652006624105d54
Andrew McClellan <andrew.mcclellan3@gmail.com>
2014-10-14 19:39:06 -0700

    Controller and view working. Model has bugs.

--- Widget/public/js/application.js
+++ Widget/public/js/application.js
@@ -3,49 +3,48 @@
     // controller
 
     var MarkdownWidget = function(source,preview){
+
       return{
         addListener: function(){
           $(source).on("keyup", function(){
             codeToTranslate = $(this).val();
-            view(preview)
+            view(codeToTranslate, preview)
+            console.log("I am the preview in controller " + preview)
           })
         }
       };
-    }
-
-
-    // controller 
+    };
 
     // view
     var view = function(preview){
       $(preview).append(translator(codeToTranslate))
+      console.log("I am the preview in the view " + preview )
       };
-    // view
 
-// model
+    // model
     var translator = function(codeToTranslate,preview) {
-
+      console.log("I am the preview in the translator " + preview)
        if ((codeToTranslate.charAt(0) === "*") && (codeToTranslate.charAt(1) === "*") && (codeToTranslate.charAt(codeToTranslate.length-1) === "*") && (codeToTranslate.charAt(codeToTranslate.length-2) === "*")) {
-        $(preview).empty();
+        // $(preview).empty();
         return "<p><b>" + codeToTranslate.substr(2, (codeToTranslate.length-4)) + "</b></p>"
       }
 
        else if ((codeToTranslate.charAt(0) === "_") && (codeToTranslate.charAt(1) === "_") && (codeToTranslate.charAt(codeToTranslate.length-1) === "_") && (codeToTranslate.charAt(codeToTranslate.length-2) === "_")) {
-        $(preview).empty();
+        // $(preview).empty();
         return "<p><b>" + codeToTranslate.substr(2, (codeToTranslate.length-4)) + "</b></p>"
       }
 
       else if ((codeToTranslate.charAt(0) === "*") && (codeToTranslate.charAt(codeToTranslate.length - 1) === "*")) {
-        $(preview).empty();
+        // $(preview).empty();
         return "<p><em>" + codeToTranslate.substr(1, (codeToTranslate.length-2)) + "</em></p>"
       }
 
       else if ((codeToTranslate.charAt(0) === "_") && (codeToTranslate.charAt(codeToTranslate.length - 1) === "_")) {
-        $(preview).empty();
+        // $(preview).empty();
         return "<p><em>" + codeToTranslate.substr(1, (codeToTranslate.length-2)) + "</em></p>"
       }
       else {
-        $(preview).empty();
+        // $(preview).empty();
       }
     };
 
